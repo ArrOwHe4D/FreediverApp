c67937763435fb4bee95d966d16d8aaa6206781a
MarcUnknown <67115582+MarcUnknown@users.noreply.github.com>
2020-11-29 18:57:44 +0100

    BluetoothDevices

---
+++ BluetoothDevicesActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FreediverApp
+{
+    [Activity(Label = "@string/app_name")]
+    public class BluetoothDevicesActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.BluetoothDevicesPage);
+        }
+    }
+}
