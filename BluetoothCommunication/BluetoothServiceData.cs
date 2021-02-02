eed2f26674b1e880b45f16307b36fe2a96dace5d
Manuel Bäcker <manuel.b.97@gmx.de>
2021-02-02 19:57:51 +0100

    fix characteristic uuid

--- BluetoothCommunication/BluetoothServiceData.cs
+++ BluetoothCommunication/BluetoothServiceData.cs
@@ -14,6 +14,6 @@ namespace FreediverApp.BluetoothCommunication
     public static class BluetoothServiceData
     {
         public static string DIVE_SERVICE_ID = "19B10000-E8F3-537E-4F6C-D194768A2214";
-        public static string DIVE_CHARACTERISTIC_ID = "19B10001-E8F2-537E-4F7C-D104768A1214";
+        public static string DIVE_CHARACTERISTIC_ID = "19B10001-E8F2-537E-4F6C-D104768A1214";
     }
 }
--- BluetoothCommunication/DiveDataConverter.cs
+++ BluetoothCommunication/DiveDataConverter.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using Org.Json;
 
 namespace FreediverApp.BluetoothCommunication
 {
